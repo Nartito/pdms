8405edbd25981860b7d4df0174406b52b3237937
Nart Shufta <nartishufta@gmail.com>
2022-05-04 00:12:45 +0200

    commit

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import * as AiIcons from "react-icons/ai";
-import * as FaIcons from "react-icons/fa";
-import * as IoIcons from "react-icons/io";
 
 
 export const SidebarData = [
--- src/pages/Staff.js
+++ src/pages/Staff.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
 
 export class Staff extends Component{
-    render(){
 
+    constructor(props){
+        super(props);
+        this.state={deps:[]}
+    }
+
+    refreshList(){
+        fetch(process.env.REACT_APP_API+'staff')
+        .then(response => response.json())
+        .then(data => {this.setState({deps:data})})
+    }
+
+    componentWillMount(){
+        this.refreshList();
+    }
+
+    componentDidUpdate(){
+        this.refreshList();
+    }
+
+
+    render(){
+        const {deps} = this.state;
         return(
             <div className='staff'>
-                <h1>Staff</h1>
+                <Table className='mt-4' striped bordered hover size='sm' >
+                    <thead>
+                        <tr>ID</tr>
+                        <tr>Name</tr>
+                        <tr>Surname</tr>
+                        <tr>Rank</tr>
+                        <tr>Birthdate</tr>
+                        <tr>DoJ</tr>
+                        <tr>OPTIONS</tr>
+                    </thead>
+                    <tbody>
+                        {deps.map(dep => <tr key={dep.ID}>
+                            <td>{dep.Id}</td>
+                            <td>{dep.Name}</td>
+                            <td>{dep.Surname}</td>
+                            <td>{dep.Rank}</td>
+                            <td>{dep.Birthday}</td>
+                            <td>{dep.DateOfJoining}</td>
+                            <td>EDIT / DELETE</td>
+                        </tr>)}
+                    </tbody>
+                </Table>
             </div>
         )
     }
